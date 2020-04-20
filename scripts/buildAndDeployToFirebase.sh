876cf2f101580ee9b4e2a4c819dfe5074c393a49
Karol Depka <karol-depka@users.noreply.github.com>
2020-04-20 04:20:14 +0200

    buildAndDeployToFirebase.sh : add git wd clean check, run testcafe on lws-hosted and deployed

--- scripts/buildAndDeployToFirebase.sh
+++ scripts/buildAndDeployToFirebase.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 doAll () {
+  if [[ -z "$(git status --porcelain)" ]]; then
+    echo "Git Working directory clean"
+  else
+    echo
+    echo "ERROR: Uncommitted changes - unable will not deploy"
+    echo
+    git status
+    exit 1
+  fi
   echo Starting build and deploy `date`
   set -x
 
@@ -14,12 +23,17 @@ doAll () {
   #git tag test_`date --utc +%Y-%m-%d_%H.%M.%SZ`
 
   #ng build \
+  npm  lws  &
+
+  export TESTCAFE_URL="http://localhost:8811/" # lws
   time \
-    scripts/testCafe.sh \
-    && npm  run  build.prod \
-    && time  firebase deploy --only hosting \
-    && git tag deploy_`date -u +%Y-%m-%d__%H.%M.%SZ` \
-    && git push --tags
+    scripts/testCafe.sh  --debug-on-fail  \
+    &&  npm  run  build.prod  \
+    &&  time  firebase deploy --only hosting  \
+    &&  export TESTCAFE_URL="https://lifedvisor.innotopic.com/"  \
+    &&  git tag deploy_`date -u +%Y-%m-%d__%H.%M.%SZ`  \
+    &&  git push --tags  \
+    &&  scripts/testCafe.sh  --debug-on-fail
 
   Echo Finished "$0" `date`
 }
